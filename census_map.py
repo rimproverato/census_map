9044306f4a5e78a51cdbd3cd0a0ebc3b2d1cb178
David <mwikyakimolo@gmail.com>
2019-11-26 17:30:20 +0300

    This displays the markers of all places on the map

--- census_map.py
+++ census_map.py
@@ -7,7 +7,17 @@ countries_locations = pandas.read_csv("countries.csv")
 lat = list(countries_locations["latitude"])
 lon = list(countries_locations["longitude"])
 
+# trying to convert to numeric so that i do not have any 'Naans'
+lt = pandas.to_numeric(lat)
+lg = pandas.to_numeric(lon)
+
 # Looping through the countries 
 geo_map = folium.Map(location=[8.7832, 34.5085], zoom_start=3.8)
+background = folium.FeatureGroup(name= "Population map")
+for lt, lg in zip(lat, lon):
+    background.add_child(folium.Marker(location= [lt , lg], popup= "population: ", icon=folium.Icon(color="blue")))
+
+# Rendering, displaying to the web
+geo_map.add_child(background)
 geo_map.save("map.html")
 
