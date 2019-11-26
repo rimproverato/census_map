8b7af6bc21ee966edd4f6167fbc4088329bbd1c9
David <mwikyakimolo@gmail.com>
2019-11-26 16:37:22 +0300

    fixed error

--- census_map.py
+++ census_map.py
@@ -1,8 +1,13 @@
+# Importing required libraries
 import folium
 import pandas
 
+# Getting the longitude and latitude of all the countries
+countries_locations = pandas.read_csv("countries.csv")
+lat = list(countries_locations["latitude"])
+lon = list(countries_locations["longitude"])
+
+# Looping through the countries 
 geo_map = folium.Map(location=[8.7832, 34.5085], zoom_start=3.8)
 geo_map.save("map.html")
 
-data = pandas.read_csv("countries.csv")
-print(data)
